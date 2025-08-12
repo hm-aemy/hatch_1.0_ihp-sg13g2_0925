4abfc6ea1abfcd4b47a968b0fc6f6b039ceeb659
Wallentowitz, Stefan <stefan.wallentowitz@hm.edu>
2025-08-12 13:53:58 +0200

    Croc with padring

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
 
-#ciel is not setup for ihp yet, it need to be clone directly
-git clone https://github.com/IHP-GmbH/IHP-Open-PDK.git /opt/IHP-Open-PDK 2> /dev/null || git -C /opt/IHP-Open-PDK pull
-# We have to use our fork branch of librelane. Just meanwhil the ihp branch of the librelane is not merge to the dev branch
-git clone -b try_merge https://github.com/hm-aemy/librelane /opt/librelane-hm-aemy 2> /dev/null || git -C /opt/librelane-hm-aemy pull
+git -C /opt/librelane fetch origin
+git -C /opt/librelane checkout leo/padring
+git -C /opt/librelane pull
 
-git -C /opt/librelane checkout dev
-git -C /opt/IHP-Open-PDK/ checkout dev
+git clone -b leo/padring https://github.com/mole99/IHP-Open-PDK.git /opt/IHP-Open-PDK-hm-aemy 2> /dev/null || git -C /opt/IHP-Open-PDK-hm-aemy pull
+#git clone -b https://github.com/librelane/librelane.git /opt/librelane 2> /dev/null || git -C /opt/librelane pull
 
-#nix-shell --pure /opt/librelane --run "librelane --run-tag hatch_1.0_ihp-sg13g2_0925 --overwrite config.json"
-nix-shell --pure /opt/librelane-hm-aemy --run "PDK_ROOT=/opt/IHP-Open-PDK/ PDK=ihp-sg13g2 librelane --manual-pdk --run-tag hatch_1.0_ihp-sg13g2_0925 --overwrite config.json"
+#git -C /opt/librelane checkout leo/padring
+#git -C /opt/librelane pull
+
+nix-shell --pure /opt/librelane --run "PDK_ROOT=/opt/IHP-Open-PDK-hm-aemy/ PDK=ihp-sg13g2 librelane --manual-pdk --run-tag croc_pl_60_2100 --overwrite config.json"
